e23ba6adfca7d177f582bdd15067865496952133
melissa-bresler <108290826+melissa-bresler@users.noreply.github.com>
2026-01-27 19:48:48 +0000

    Update Game Model (#20)

--- Backend.Api/Models/Game.cs
+++ Backend.Api/Models/Game.cs
@@ -2,12 +2,12 @@ namespace Backend.Api.Models;
 
 public class Game
 {
-    public int Id { get; set; }
+    public Guid id { get; set; }
 
-    public string Slug { get; set; } = string.Empty;
+    public string slug { get; set; } = string.Empty;
 
-    public string Title { get; set; } = string.Empty;
+    public string title { get; set; } = string.Empty;
 
-    public string Description { get; set; } = string.Empty;
+    public string description { get; set; } = string.Empty;
 
 }
